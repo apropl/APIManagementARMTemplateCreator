8917f02a758bfc946ca5b2b6bcc699c6bd07bd32
Richard Tomstad <richard.tomstad@mtr.se>
2023-10-06 11:22:05 +0200

    added handling of nextlinks. Implemented because all named values where not exported... Could of course be done nicer but does the work... have no more time to put on this...

--- APIManagementTemplate/AzureResourceCollector.cs
+++ APIManagementTemplate/AzureResourceCollector.cs
@@ -6,8 +6,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace APIManagementTemplate
 {
@@ -41,26 +43,56 @@ namespace APIManagementTemplate
         {
             string url = resourceId + $"{GetSeparatorCharacter(resourceId)}api-version={apiversion}" + (string.IsNullOrEmpty(suffix) ? "" : $"&{suffix}");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var response = await client.GetAsync(url);
-
-            if (response.StatusCode == HttpStatusCode.NotFound)
+            HttpResponseMessage response;
+            string responseContent;
+            JArray finalJArray =  new JArray();
+            JObject finalResponse = new JObject();
+            var mergeSettings = new JsonMergeSettings
             {
-                return null;
-            }
-            
-            var responseContent = await response.Content.ReadAsStringAsync();
-            if (response.StatusCode == HttpStatusCode.Forbidden)
-            {
-                throw new UnauthorizedAccessException(responseContent);
-            }
+                MergeArrayHandling = MergeArrayHandling.Union
+            };
+            JToken nextLinkJToken;
 
+            do
+            {
+                response = await client.GetAsync(url);
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+
+                responseContent = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    throw new UnauthorizedAccessException(responseContent);
+                }
+
+                finalResponse = JObject.Parse(responseContent);
+
+                var valueArr = JObject.Parse(responseContent).SelectToken("value");
+                if (valueArr != null && valueArr.Type == JTokenType.Array)
+                {
+                    finalJArray.Merge(JObject.Parse(responseContent).SelectToken("value") as JArray, mergeSettings);
+                    finalResponse.SelectToken("value").Replace(finalJArray);
+                }
+
+                //Check if nextLink
+                nextLinkJToken = JObject.Parse(responseContent).SelectToken("nextLink");
+                if (nextLinkJToken != null)
+                {
+                    //Update URL with nextlink
+                    url = nextLinkJToken.ToString();
+                }
+
+            } while (nextLinkJToken != null);
 
             if (!string.IsNullOrEmpty(DebugOutputFolder))
             {
                 var path = DebugOutputFolder + "\\" + EscapeString(resourceId.Split('/').SkipWhile( (a) => { return a != "service" && a != "workflows" && a != "sites"; }).Aggregate<string>((b,c) => { return b +"-" +c; })  + ".json");
-                System.IO.File.WriteAllText(path, responseContent);
+                System.IO.File.WriteAllText(path, finalResponse.ToString());
             }
-            return JObject.Parse(responseContent);
+            return finalResponse;
 
         }
 
